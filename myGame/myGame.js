bf6b18d3be1c9bbf63e4e6f734f491c8261c0a70
Mateo Quiros <mquiros.sh@mysummitps.org>
2016-11-07 20:12:47 +0000

    update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -5,28 +5,44 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 var game_state = {}
 
 
-game_state.main = function () {};
+game_state.main = function() {};
 game_state.main.prototype = {
 
 
-	preload: function() {
+    preload: function() {
+        game.load.image('sky', 'assets/sky.png');
+        game.load.image('ground', 'assets/platform.png');
+        game.load.image('star', 'assets/star.png');
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
+
+
+    },
+
+
+    create: function() {
+        game.add.sprite(0, 0, 'star');
+        game.add.sprite(0, 0, 'sky');
+        
+        this.platforms = game.add.group();
+        this.platforms.enablebody = true;
+        
+        var ground = this.platforms.create(0, game.world.height - 64, 'ground');
+        ground.scale.setTo(2, 2)
+        ground.body.immovable = true;
+        var ledge = this.platforms.create(200, 300, 'ground');
+        ledge.body.immovable = true;
+        game.physics.startsystem(phaser.physics.ARCADE);
         
 
 
-
-},
-
-
-create: function() {
+    },
 
 
-},
+    update: function() {
 
 
-update: function() {
-
-
-},
+    },
 
 
 }
